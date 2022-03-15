5747efebcf11ca47aab85b93218633a351ff0c8e
Joaqim Planstedt <git@joaqim.xyz>
2022-03-15 11:09:03 +0100

    update: tsconfig.json & webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,7 +7,6 @@ module.exports = merge(common, {
     mode: "development",
     devtool: "inline-source-map",
     devServer: {
-        contentBase: path.resolve(__dirname, "./dist"),
         compress: true,
         port: 9000,
     },
