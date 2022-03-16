15186355e1ba392fd84bbf7c4727ad68860150a3
Joaqim Planstedt <git@joaqim.xyz>
2022-03-16 13:37:21 +0100

    minor fixes and additions

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,7 +5,16 @@ const common = require("./webpack.config.common")
 
 module.exports = merge(common, {
     mode: "development",
-    devtool: "inline-source-map",
+    //devtool: "inline-source-map",
+    devtool: "cheap-source-map",
+    optimization: {
+      removeAvailableModules: false,
+      removeEmptyChunks: false,
+      splitChunks: false
+    },
+    output: {
+      pathinfo: false,
+    },
     devServer: {
         compress: true,
         port: 9000,
